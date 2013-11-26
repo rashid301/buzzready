78daf9644bfef4927f9aaf3a02ae2cbe315d8a31
rashid301 <rashid301@d29a48b6-6a26-fd34-57d9-ac11c3340f9d>
2013-11-26 01:03:10 +0000

    implemented UI, session management, registration, login

---
+++ trunk/client/src/org/client/buzzready/Constants.java
@@ -0,0 +1,13 @@
+package org.client.buzzready;
+
+public class Constants {
+	public final static int NO_NETWORK = 3;
+	public final static int PASSWORD_MISMATCH = 2;
+	public final static int USERNAME_EXITS = 1;
+	public final static int SUCCESS = 200;
+	public static final int USERNAME_PASSWORD_INVALID = 4;
+	public static final int LOGIN_OPER = 1;
+	public static final int REGISTER_OPER = 2;
+	public static final int INCOMPLETE_FORM = 5;
+
+}
--- trunk/client/src/org/client/buzzready/LoginActivity.java
+++ trunk/client/src/org/client/buzzready/LoginActivity.java
@@ -1,70 +1,152 @@
 package org.client.buzzready;
 
-import jim.h.common.android.lib.zxing.integrator.IntentIntegrator;
-import jim.h.common.android.lib.zxing.integrator.IntentResult;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
+	private EditText username = null;
+	private EditText password = null;
+	private TextView status = null;
+	private SessionManager sessionmanager = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_login);
-		
-		TextView username = (TextView)findViewById(R.id.username);
+
+		username = (EditText) findViewById(R.id.username);
 		username.requestFocus();
 
-		TextView password = (TextView)findViewById(R.id.password);
-		
-		Button btn_login = (Button)findViewById(R.id.btn_login);
-		
+		password = (EditText) findViewById(R.id.password);
+
+		status = (TextView) findViewById(R.id.status_login);
+
+		Button btn_login = (Button) findViewById(R.id.btn_login);
+
+		Button btn_register = (Button) findViewById(R.id.btn_register);
+
 		btn_login.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				//start the login process
-				// TODO Auto-generated method stub
-				
+				if (!isNetworkAvailable()) {
+					updateView(Constants.NO_NETWORK);
+					return;
+				}
+				if (username.getText().toString().isEmpty()
+						|| password.getText().toString().isEmpty()) {
+					Log.i("LoginActivity", "Incompleteform");
+					updateView(Constants.INCOMPLETE_FORM);
+					return;
+				}
+				LoginAsync process = new LoginAsync();
+				process.execute();
+
 			}
 		});
 
-		
-		Button btn_register = (Button)findViewById(R.id.btn_register);
 		btn_register.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(LoginActivity.this, Register.class);
+				Intent intent = new Intent(LoginActivity.this,
+						RegisterActivity.class);
+				intent.putExtra("username", username.toString());
 				startActivity(intent);
+
 			}
 		});
 	}
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		switch (requestCode) {
-		case IntentIntegrator.REQUEST_CODE:
-			IntentResult scanResult = IntentIntegrator.parseActivityResult(
-					requestCode, resultCode, data);
-			if (scanResult == null) {
-				return;
-			}
-			final String result = scanResult.getContents();
-			if (result != null){
-				Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-				intent.putExtra("result", result);
-				startActivity(intent);
-			}
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
 
+		sessionmanager = SessionManager.getInstance(getApplicationContext());
+
+		if (sessionmanager.isLoggedIn()) {
+			changeActiviy();
+			return;
 		}
 
+	}
+
+	private void updateView(int status) {
+
+		switch (status) {
+		case Constants.USERNAME_PASSWORD_INVALID:
+			this.status.setTextColor(getResources().getColor(R.color.dark_red));
+			this.status.setText("username/password invalid");
+			break;
+
+		case Constants.SUCCESS:
+			this.status
+					.setTextColor(getResources().getColor(R.color.dark_blue));
+			this.status.setText("Registration Successful");
+			sessionmanager.createLoginSession(username.getText().toString());
+			changeActiviy();
+			break;
+
+		case Constants.NO_NETWORK:
+			this.status.setTextColor(getResources().getColor(R.color.dark_red));
+			this.status.setText("No Internet Connectivity");
+			break;
+
+		case Constants.INCOMPLETE_FORM:
+			this.status.setTextColor(getResources().getColor(R.color.dark_red));
+			this.status.setText("Please fill all fields");
+			break;
+		}
+
+		this.status.setVisibility(View.VISIBLE);
+	}
+
+	private class LoginAsync extends AsyncTask<Void, Void, Integer> {
+
+		@Override
+		protected Integer doInBackground(Void... params) {
+			HttpConnection hc = HttpConnection.getInstance();
+			return hc.registerOrLogin(Constants.LOGIN_OPER,
+					username.toString(), password.toString());
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			// updateView(result.intValue());
+			updateView(Constants.SUCCESS);
+		}
+	}
+
+	private void changeActiviy() {
+		Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+		intent.putExtra("username", username.toString());
+		startActivity(intent);
+		finish();
+	}
+
+	public boolean isNetworkAvailable() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+		// if no network is available networkInfo will be null
+		// otherwise check if we are connected
+		if (networkInfo != null && networkInfo.isConnected()) {
+			return true;
+		}
+		return false;
 	}
 
 }
